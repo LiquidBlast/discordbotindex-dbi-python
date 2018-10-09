c8839d0f4a6aa5fa58601e9d5bb65d5c0fb0536b
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-09 19:40:15 -0400

    Fix class error

--- dbipyt/dbipyt.py
+++ dbipyt/dbipyt.py
@@ -1,15 +1,14 @@
 import aiohttp
 
 class dbipyt:
-    def __init__(self):
 
-    async def post(self, token, bot_id, guild_count):
+    async def post(token, bot_id, guild_count):
         async with aiohttp.ClientSession(headers={'Authorization': token}) as s:
             async with s.post(f"https://discordbotindex.com/apiv1/bot/{bot_id}", data={'server_count': guild_count}) as r:
                 r = await r.json()
                 return r
 
-    async def fetch(self, bot_id):
+    async def fetch(bot_id):
         async with aiohttp.ClientSession() as s:
             async with s.get(f"https://discordbotindex.com/apiv1/bot/{bot_id}") as r:
                 r = await r.json()
