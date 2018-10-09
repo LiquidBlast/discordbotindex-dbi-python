0ac22c08bdd1d250a32669f2d09f7bd591d9506a
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-10-09 19:32:33 -0400

    Update args

--- example.py
+++ example.py
@@ -7,12 +7,12 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 @commands.is_owner()
 async def post(ctx):
-    r = await dbipyt.post(token="your-token-here", bot_id="bot-id-here", guild_count=len(bot.guilds))
+    r = await dbipyt.dbipyt.post(token="your-token-here", bot_id="bot-id-here", guild_count=len(bot.guilds))
     await ctx.send(f"Posted! Reponse: {r}")
     
 @bot.command()
 async def botinfo(ctx, bot_id):
-    r = await dbipyt.fetch(bot_id=bot_id)
+    r = await dbipyt.dbipyt.fetch(bot_id=bot_id)
     await ctx.send(f"Prefix: {r['prefix']}, Description: {r['shortdesc']}, Upvotes: {r['upvotes']}")
     # https://discordbotindex.com/documentation
 
