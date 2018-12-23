047d4c9b41c6095c360db91de5388faaf6ef30f5
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-23 12:03:55 -0500

    Add fancy new error system

--- dbipyt/dbipyt.py
+++ dbipyt/dbipyt.py
@@ -1,28 +1,31 @@
 import aiohttp
-import json
+from .errors import InvalidEndpoint, InvalidToken, InvalidClientID
 
 class Client:
     def __init__(self, token):
         self.auth = token
 
     async def post(self, bot_id:str=None, guild_count:int=None):
-        async with aiohttp.ClientSession(headers={'Authorization': self.auth}) as s:
+        async with aiohttp.ClientSession(headers={'Authorization': 'e'}) as s:
             async with s.post(f"https://discordbotindex.com/apiv1/bot/{bot_id}", data={'server_count': guild_count}) as r:
                 r = await r.json()
-                return r
+                if r == {"error": "unauthorized"}:
+                    raise InvalidToken("You provided an invalid token.")
+                else:
+                    return r
 
-#class Fetch:
 async def fetch(bot_id, endpoint:str=None):
     async with aiohttp.ClientSession() as s:
         async with s.get(f"https://discordbotindex.com/apiv1/bot/{bot_id}") as r:
             r = await r.json()
-            if endpoint is None:
-                try:
-                    return r
-                except:
-                    return "[dbipyt.0.1.2] Invalid client ID. Please provide a client ID with the bot_id argument."
+            if r == {"error":"unknown_bot"}:
+                raise InvalidClientID(f"'{bot_id}' is not a valid ID. Please provide a correct client ID.")
+            elif r == {"error":"param_must_be_int"}:
+                raise InvalidClientID("Client IDs must be in int.")
+            elif endpoint is None:
+                return r
             else:
                 try:
                     return r[endpoint]
-                except KeyError as e:
-                    return f"[dbipyt.0.1.2] Invalid endpoint: {e}"
+                except KeyError as error:
+                    raise InvalidEndpoint(error)
