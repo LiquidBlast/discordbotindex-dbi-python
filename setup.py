656ba488bc4c7cb90b3570c4c271148846381844
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-22 21:26:27 -0500

    Update Version

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from distutils.core import setup
 setup(
   name = 'dbipyt',
   packages = ['dbipyt'],
-  version = '0.1.1',
+  version = '0.1.2',
   license= 'MIT',
   description = 'API Wrapper for Discord Bot Index [GUILD COUNT POSTING, BOT INFO SEARCH]',
   author = 'Luke J',
   author_email = 'itsnotluke@outlook.com',
-  url = 'https://github.com/paixlukee/DBI-Python',
-  download_url = 'https://github.com/paixlukee/DBI-Python/archive/v_0.01.tar.gz',
+  url = 'https://github.com/LiquidBlast/discordbotindex-dbi-python',
+  download_url = 'https://github.com/LiquidBlast/discordbotindex-dbi-python/archive/v_0.01.tar.gz',
   keywords = ['apiwrapper', 'dbi', 'discordbotindex'],
   install_requires=[
           'aiohttp',
           'json'
       ],
   classifiers=[
-    'Development Status :: Beta',
+    'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
