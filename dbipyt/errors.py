21a908e90f19e784fb141fa7225f7d2b56cd53ed
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-23 12:02:43 -0500

    Add error system

---
+++ dbipyt/errors.py
@@ -0,0 +1,17 @@
+class InvalidEndpoint(Exception):
+    """
+    Exception occurs when the endpoint given is not valid
+    """
+    pass
+
+class InvalidToken(Exception):
+    """
+    Exception occurs when the token given isn't valid
+    """
+    pass
+
+class InvalidClientID(Exception):
+    """
+    Exception occurs when the bot/client ID given is not valid
+    """
+    pass
