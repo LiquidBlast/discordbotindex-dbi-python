1c2a3c91139f9d600cd03900c8319d5af468f41a
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-22 21:09:02 -0500

    Update Fetch & Post

--- dbipyt/dbipyt.py
+++ dbipyt/dbipyt.py
@@ -3,26 +3,26 @@ import json
 
 class Client:
     def __init__(self, token):
-	self.auth = token
+        self.auth = token
 
-    async def post(bot_id, guild_count):
+    async def post(self, bot_id:str=None, guild_count:int=None):
         async with aiohttp.ClientSession(headers={'Authorization': self.auth}) as s:
             async with s.post(f"https://discordbotindex.com/apiv1/bot/{bot_id}", data={'server_count': guild_count}) as r:
                 r = await r.json()
                 return r
-class Fetch:
-	
-    async def fetch(bot_id, endpoint:str=None):
-        async with aiohttp.ClientSession() as s:
-            async with s.get(f"https://discordbotindex.com/apiv1/bot/{bot_id}") as r:
-                r = await r.json()
-                if endpoint is None:
-                    try:
-                        return r
-                    except:
-                        return "[dbipyt.0.1.1] Invalid client ID. Please provide a client ID with the bot_id argument."
-                else:
-                    try:
-                        return r[endpoint]
-                    except KeyError as e:
-                        return f"[dbipyt.0.1.1] Invalid endpoint: \"{e}\""
+
+#class Fetch:
+async def fetch(bot_id, endpoint:str=None):
+    async with aiohttp.ClientSession() as s:
+        async with s.get(f"https://discordbotindex.com/apiv1/bot/{bot_id}") as r:
+            r = await r.json()
+            if endpoint is None:
+                try:
+                    return r
+                except:
+                    return "[dbipyt.0.1.1] Invalid client ID. Please provide a client ID with the bot_id argument."
+            else:
+                try:
+                    return r[endpoint]
+                except KeyError as e:
+                    return f"[dbipyt.0.1.1] Invalid endpoint: {e}"
