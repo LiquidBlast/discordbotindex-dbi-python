83e54a87d61389b53d9e61efb812763aeb5f8536
Luke J <36803432+paixlukee@users.noreply.github.com>
2018-12-22 21:19:38 -0500

    Updated example to fit 0.1.2

--- example.py
+++ example.py
@@ -7,14 +7,25 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 @commands.is_owner()
 async def post(ctx):
-    r = await dbipyt.dbipyt.post(token="your-token-here", bot_id="bot-id-here", guild_count=len(bot.guilds))
+    
+    client = dbipyt.Client(token='token')
+    r = await client.post(bot_id='id', guild_count=len(bot.guilds))
     await ctx.send(f"Posted! Reponse: {r}")
     
 @bot.command()
 async def botinfo(ctx, bot_id):
-    r = await dbipyt.dbipyt.fetch(bot_id=bot_id)
+    
+    # to get the whole JSON response..
+    r = await dbipyt.fetch(bot_id='id')
     await ctx.send(f"Prefix: {r['prefix']}, Description: {r['shortdesc']}, Upvotes: {r['upvotes']}")
-    # https://discordbotindex.com/documentation
+                   
+    # to get each endpoint..
+    prefix = await dbipyt.fetch(bot_id='id', endpoint='prefix') # endpoints: https://discordbotindex.com/documentation
+    short_description = await dbipyt.fetch(bot_id='id', endpoint='shortdesc')
+    upvotes = await dbipyt.fetch(bot_id='id', endpoint='upvotes')
+    await ctx.send(f"Prefix: {prefix}, Description: {shortdesc}, Upvotes: {upvotes}")
+    
+                  
 
 
 bot.run('token')
